d9e517314868e8a791a2c954e2c85db4894e661a
El <lomashonok@inbox.lv>
2017-09-13 19:06:05 +0300

    TimeTester

--- IrisForm/Tools/Program.cs
+++ IrisForm/Tools/Program.cs
@@ -5,8 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Helpers {
+
     class Program {
         static void Main(string[] args) {
+
+            TimeTester test = new TimeTester("Test");
+
+            test.SetTimer();
+
+            Console.WriteLine("Don't panic! I'm calculating pi 2 000 000 000 times to test TimeTester class...");
+            for (int i = 0; i < 2000000000; i++)
+            {
+                float pi = 82 / 26;
+            }
+
+            test.StopTimer();
+
+            Console.WriteLine(test.GetStringTime());
+            test.WriteToFile();
+
+            Console.WriteLine("That's all!");
+            Console.ReadKey();
         }
     }
 }
--- IrisForm/Tools/TimeTester.cs
+++ IrisForm/Tools/TimeTester.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,25 +12,62 @@ namespace Helpers
         private DateTime timerStart;
         private DateTime timerEnd;
         private TimeSpan timeSpan;
+        private string name;
+        
+        public string FileName;
+        public bool AddNewToFile;
+
+        public TimeTester(string name, bool addNewToFile = true)
+        {
+            this.name = name;
+            this.AddNewToFile = addNewToFile;
+        }
 
         public void SetTimer()
         {
-            timerStart = new DateTime();
+            timerStart = DateTime.Now;
         }
 
         public void StopTimer()
         {
-            timerEnd = new DateTime();
+            timerEnd = DateTime.Now;
         }
 
-        //public TimeSpan GetTime()
-        //{
-        //    try
-        //    {
+        public TimeSpan GetTime()
+        {
+            try
+            {
+                if (timerStart != null)
+                {
+                    if (timerEnd == null)
+                        StopTimer();
 
-        //    }
-        //    catch ()
+                    timeSpan = timerEnd - timerStart;
+                    return timeSpan;
+                }
+                return TimeSpan.Zero;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
 
-        //}
+        public string GetStringTime()
+        {
+            return (name + " : " + GetTime());
+        }
+
+        public void WriteToFile()
+        {
+            StreamWriter Writer;
+            if (FileName != null && FileName != String.Empty)
+                Writer = new StreamWriter(FileName, AddNewToFile);
+            else
+                Writer = new StreamWriter(name + ".txt", AddNewToFile);
+
+            Writer.WriteLine(GetStringTime());
+            Writer.Close();
+        }
     }
 }
