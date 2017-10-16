933bc2b69a079dc4ed8ac256bf81acaa3477eab4
Kirill Raduntsev <raduncevkirill@gmail.com>
2017-10-16 19:14:33 +0300

    statica

--- IrisForm/Tools/Recolor.cs
+++ IrisForm/Tools/Recolor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 
 namespace Tools
 {
@@ -14,7 +15,7 @@ namespace Tools
         /// </summary>
         /// <returns>Bitmap picture in grey</returns>
         /// <param name="original">Original picture, passed to convert.</param>
-        public Bitmap ToGrey(Bitmap original)
+        public static Bitmap ToGrey(Bitmap original)
         {
             var greyedPicture = new Bitmap(original.Width, original.Height);
 
@@ -28,6 +29,29 @@ namespace Tools
             }
 
             return greyedPicture;
+        }
+
+        public static Image Resize(Image image, int newWidth, int maxHeight, bool onlyResizeIfWider) {
+            if (onlyResizeIfWider && image.Width <= newWidth) newWidth = image.Width;
+
+            var newHeight = image.Height * newWidth / image.Width;
+            if (newHeight > maxHeight) {
+                // Resize with height instead  
+                newWidth = image.Width * maxHeight / image.Height;
+                newHeight = maxHeight;
+            }
+
+            var res = new Bitmap(newWidth, newHeight);
+
+            using (var graphic = Graphics.FromImage(res)) {
+                graphic.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphic.SmoothingMode = SmoothingMode.HighQuality;
+                graphic.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                graphic.CompositingQuality = CompositingQuality.HighQuality;
+                graphic.DrawImage(image, 0, 0, newWidth, newHeight);
+            }
+
+            return res;
         }
     }
 }
